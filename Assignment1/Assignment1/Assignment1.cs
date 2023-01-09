2e9b3faed35c680937f53e9c9ada4676e2964894
DESKTOP-975US36\mh <akehrn13@gamail.com>
2023-01-09 12:42:08 +0900

    20230109 Assignment(1)

---
+++ Assignment1/Assignment1/Assignment1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.IO;
+
+namespace Assignment1
+{
+    public static class Assignment1
+    {
+        public static void PrintIntegers(StreamReader input, StreamWriter output, int width)
+        {
+            StreamReader streamInput = input;
+            char[] streamChars = streamInput;
+        }
+
+        public static void PrintStats(StreamReader input, StreamWriter output)
+        {
+
+        }
+    }
+}
--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.IO;
+using System.Text;
 
 namespace Assignment1
 {
@@ -6,7 +8,22 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string integerInput = "52\n102\n1\n2382\n409"; // PrintIntegers() Auto input
+            string floatInput = "2.23\n-1912.87233125\n2323\n1\n34.55567"; // PrintStats() Auto input
+            byte[] buffer1 = Encoding.UTF8.GetBytes(integerInput);
+            byte[] buffer2 = Encoding.UTF8.GetBytes(floatInput);
+
+            using (StreamReader READER1 = new StreamReader(new MemoryStream(buffer1)))
+            using (StreamReader READER2 = new StreamReader(new MemoryStream(buffer2)))
+            using (StreamWriter WRITER = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true })
+            {
+                Console.WriteLine("PrintIntegers()\n");
+                Assignment1.PrintIntegers(READER1, WRITER, 14); // width : 14
+
+                Console.WriteLine("\nPrintStats()\n");
+                Assignment1.PrintStats(READER2, WRITER);
+            }
         }
+
     }
 }
