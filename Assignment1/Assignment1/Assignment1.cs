6e25b7dec0de482515fef6bd6d9a2b5b2cd2cafa
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-01-10 05:49:33 +0900

    메인함수에 있는 문자열을 Assignment1 클래스에서 출력함

--- Assignment1/Assignment1/Assignment1.cs
+++ Assignment1/Assignment1/Assignment1.cs
@@ -7,8 +7,16 @@ namespace Assignment1
     {
         public static void PrintIntegers(StreamReader input, StreamWriter output, int width)
         {
-            StreamReader streamInput = input;
-            char[] streamChars = streamInput;
+            string s1 = input.ReadLine();
+            string s2 = input.ReadLine();
+            string s3 = input.ReadLine();
+            string s4 = input.ReadLine();
+            string s5 = input.ReadLine();
+            output.WriteLine(Convert.ToString(s1));
+            output.WriteLine(s2);
+            output.WriteLine(s3);
+            output.WriteLine(s4);
+            output.WriteLine(s5);
         }
 
         public static void PrintStats(StreamReader input, StreamWriter output)
--- Assignment1/Assignment1/Program.cs
+++ Assignment1/Assignment1/Program.cs
@@ -1,6 +1,7 @@
-﻿using System;
+﻿using System.Diagnostics;
 using System.IO;
 using System.Text;
+using System;
 
 namespace Assignment1
 {
@@ -8,22 +9,26 @@ namespace Assignment1
     {
         static void Main(string[] args)
         {
-            string integerInput = "52\n102\n1\n2382\n409"; // PrintIntegers() Auto input
-            string floatInput = "2.23\n-1912.87233125\n2323\n1\n34.55567"; // PrintStats() Auto input
+            string integerInput = "25\n26\n10\n22\n16";
+            string floatInput = "12.5\n25.262\n12\n10008.2\n1224.2";
+
             byte[] buffer1 = Encoding.UTF8.GetBytes(integerInput);
             byte[] buffer2 = Encoding.UTF8.GetBytes(floatInput);
 
+
+            // using 문에 대해서는 아직 고민하지 마세요. 나중에 배웁니다.
             using (StreamReader READER1 = new StreamReader(new MemoryStream(buffer1)))
             using (StreamReader READER2 = new StreamReader(new MemoryStream(buffer2)))
+            //using (StreamReader READER = new StreamReader(Console.OpenStandardInput()))
+           
             using (StreamWriter WRITER = new StreamWriter(Console.OpenStandardOutput()) { AutoFlush = true })
             {
-                Console.WriteLine("PrintIntegers()\n");
-                Assignment1.PrintIntegers(READER1, WRITER, 14); // width : 14
+                Console.WriteLine("Enter your 5 digits for PrintIntegers():");
+                Assignment1.PrintIntegers(READER1, WRITER, 14);
 
-                Console.WriteLine("\nPrintStats()\n");
+                Console.WriteLine("Enter your 5 digits for PrintStats():");
                 Assignment1.PrintStats(READER2, WRITER);
-            }
+            }            
         }
-
     }
 }
