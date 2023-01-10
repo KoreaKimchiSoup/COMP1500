07a30a523c51d72f19c04263a975ef3df1edfafa
DESKTOP-975US36\mh <akehrn13@gamail.com>
2023-01-10 14:50:13 +0900

    no message

--- Assignment1/Assignment1/Assignment1.cs
+++ Assignment1/Assignment1/Assignment1.cs
@@ -7,16 +7,39 @@ namespace Assignment1
     {
         public static void PrintIntegers(StreamReader input, StreamWriter output, int width)
         {
-            string s1 = input.ReadLine();
-            string s2 = input.ReadLine();
-            string s3 = input.ReadLine();
-            string s4 = input.ReadLine();
-            string s5 = input.ReadLine();
-            output.WriteLine(Convert.ToString(s1));
-            output.WriteLine(s2);
-            output.WriteLine(s3);
-            output.WriteLine(s4);
-            output.WriteLine(s5);
+            int s1 = int.Parse(input.ReadLine());
+            int s2 = int.Parse(input.ReadLine());
+            int s3 = int.Parse(input.ReadLine());
+            int s4 = int.Parse(input.ReadLine());
+            int s5 = int.Parse(input.ReadLine());
+            output.WriteLine($"s1: {s1} -> {Convert.ToString(s1, 8)}");
+            output.WriteLine($"s1: {s1} -> {Convert.ToString(s1, 10)}");
+            output.WriteLine($"s1: {s1} -> {Convert.ToString(s1, 16)}");
+
+            Console.WriteLine();
+
+            output.WriteLine($"s2: {s2} -> {Convert.ToString(s2, 8)}");
+            output.WriteLine($"s2: {s2} -> {Convert.ToString(s2, 10)}");
+            output.WriteLine($"s2: {s2} -> {Convert.ToString(s2, 16)}");
+
+            Console.WriteLine();
+
+            output.WriteLine($"s3: {s3} -> {Convert.ToString(s3, 8)}");
+            output.WriteLine($"s3: {s3} -> {Convert.ToString(s3, 10)}");
+            output.WriteLine($"s3: {s3} -> {Convert.ToString(s3, 16)}");
+
+            Console.WriteLine();
+
+            output.WriteLine($"s4: {s4} -> {Convert.ToString(s4, 8)}");
+            output.WriteLine($"s4: {s4} -> {Convert.ToString(s4, 10)}");
+            output.WriteLine($"s4: {s4} -> {Convert.ToString(s4, 16)}");
+
+            Console.WriteLine();
+
+            output.WriteLine($"s5: {s5} -> {Convert.ToString(s5, 8)}");
+            output.WriteLine($"s5: {s5} -> {Convert.ToString(s5, 10)}");
+            output.WriteLine($"s5: {s5} -> {Convert.ToString(s5, 16)}");
+
         }
 
         public static void PrintStats(StreamReader input, StreamWriter output)
