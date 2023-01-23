25a2fa4f10eb513a2ba1934811696ce34f3a1859
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-01-24 04:30:55 +0900

    no message

---
+++ Lab4/Lab4/Calendar.cs
@@ -0,0 +1,32 @@
+﻿namespace Lab4
+{
+    public static class Calendar
+    {
+        public static bool IsLeapYear(uint year)
+        {
+            if (year % 4 == 0 && year % 100 == 0 || year % 400 == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public static int GetDaysInMonth(uint year, uint month)
+        {
+            return -1;
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
