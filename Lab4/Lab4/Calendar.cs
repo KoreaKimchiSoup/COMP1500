864e72460a2e27b4238abcc7e2de7f20569d4eac
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-01-24 05:05:41 +0900

    no message

--- Lab4/Lab4/Calendar.cs
+++ Lab4/Lab4/Calendar.cs
@@ -4,12 +4,7 @@
     {
         public static bool IsLeapYear(uint year)
         {
-            if (year == 0)
-            {
-                return false;
-            }
-
-            if (year %  4 == 0 && year % 100 == 0 && year % 400 == 0)
+            if (year % 4 == 0 && year % 100 == 0 && year % 400 != 0)
             {
                 return false;
             }
@@ -19,15 +14,24 @@
                 return true;
             }
 
-            if (year % 4 == 0 && year % 100 == 0 || year % 400 == 0)
+            if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
             {
                 return true;
+            }
+
+            if (year % 4 != 0 && year % 100 != 0 && year % 400 != 0)
+            {
+                return false;
             }
             return false;
         }
 
         public static int GetDaysInMonth(uint year, uint month)
         {
+            if (year < 9999 && month < 13)
+            {
+                //for (int i = 1; year )
+            }    
             return -1;
         }
     }
