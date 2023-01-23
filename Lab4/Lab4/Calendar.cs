c1dbaeec8432690044a2a9cc6fd2a82279a7cc1f
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-01-24 04:52:49 +0900

    no message

--- Lab4/Lab4/Calendar.cs
+++ Lab4/Lab4/Calendar.cs
@@ -4,6 +4,21 @@
     {
         public static bool IsLeapYear(uint year)
         {
+            if (year == 0)
+            {
+                return false;
+            }
+
+            if (year %  4 == 0 && year % 100 == 0 && year % 400 == 0)
+            {
+                return false;
+            }
+
+            if (year % 4 == 0 && year % 100 != 0)
+            {
+                return true;
+            }
+
             if (year % 4 == 0 && year % 100 == 0 || year % 400 == 0)
             {
                 return true;
