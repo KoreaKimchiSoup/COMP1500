b454e57abc020949dcb54f5597ce159d55cfbfb2
DESKTOP-975US36\mh <akehrn13@gamail.com>
2023-02-01 09:34:11 +0900

    no message

--- Lab5/Lab5/Lab5.cs
+++ Lab5/Lab5/Lab5.cs
@@ -9,33 +9,32 @@
 
         public static int GetInvalidEntryCount(uint[] usersPerDay, double[] revenuePerDay)
         {
+            
             return -1;
         }
 
         public static double CalculateTotalRevenue(double[] revenuePerDay, uint start, uint end)
         {
-            if (revenuePerDay.Length < start || revenuePerDay.Length < end)
+            double dNum = 0;
+
+            if (revenuePerDay.Length == 0)
+            {// 매출 데이터(요소) 수가 0일 때
+                return -1;
+            }
+
+            if (start > revenuePerDay.Length || end > revenuePerDay.Length)
             {// 시작, 혹은 끝 index가 매출 데이터 수보다 이상일 때
                 return -1;
             }
-            else if (start > end)
-            {// 시작 색인이 끝 색인보다 클 때
-                return -1;
-            }
-            else if (start < end)
-            {
-                return -1;
-            }
-            else if (revenuePerDay.Length == 0)
-            {// 매출 데이터(요소) 수가 0일 때
-                return -1;
-            }            
 
-            double dNum = 0;
-            for (uint i = start; i <= end; i++)
-            {
-                dNum += revenuePerDay[i];
+            if (start <= end)
+            {// 시작 색인이 끝 색인보다 이하일 때
+                for (uint i = start; i <= end; i++)
+                {
+                    dNum += revenuePerDay[i];
+                }
             }
+            
             return dNum;
         }
     }
