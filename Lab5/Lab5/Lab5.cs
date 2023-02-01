a97c237f353d52e9ee0f34c995146162536baab5
DESKTOP-975US36\mh <akehrn13@gamail.com>
2023-02-01 12:35:16 +0900

    no message

--- Lab5/Lab5/Lab5.cs
+++ Lab5/Lab5/Lab5.cs
@@ -3,22 +3,14 @@
     public static class Lab5
     {
         public static bool TryFixData(uint[] usersPerDay, double[] revenuePerDay)
-        {//                                  일일 사용자           일일 매출
-            /*
-               R = u / 2                  0 <= u <= 10인 경우
-               R = 16 * u / 5 - 27        10 < u <= 100인 경우
-               R = u^2 / 4 - 2*u - 2007   100 < u <= 1000인 경우
-               R = 245743 + u / 4         u > 1000인 경우
-
-               R은 매출 u는 사용자 수를 의미
-             */
-
+        {//                                  일일 사용자           일일 매출            
             return false;
         }
 
+
         public static int GetInvalidEntryCount(uint[] usersPerDay, double[] revenuePerDay)
         {
-            int invalidEntryCount = 0;
+            int InvalidEntryCount = 0;
             if (usersPerDay.Length != revenuePerDay.Length)
             {
                 return -1;
@@ -28,34 +20,33 @@
             {
                 if (usersPerDay[i] != revenuePerDay[i])
                 {
-                    invalidEntryCount++;
+                    InvalidEntryCount++;
                 }
             }
-            return invalidEntryCount;
+
+            return InvalidEntryCount;
         }
 
         public static double CalculateTotalRevenue(double[] revenuePerDay, uint start, uint end)
         {
             double dNum = 0;
 
-            if (start > revenuePerDay.Length)
-            {// 시작, index가 매출 데이터 수보다 이상일 때
+            if (revenuePerDay.Length == 0)
+            {// 매출 데이터(요소) 수가 0일 때
                 return -1;
             }
 
+            if (start > revenuePerDay.Length)
+            {// 시작, 혹은 끝 index가 매출 데이터 수보다 이상일 때
+                return -1;
+            }
             if (end > revenuePerDay.Length)
-            {// 끝 index가 매출 데이터 수보다 이상일 때
-
+            {
                 return -1;
             }
 
             if (start > end)
             {
-                return -1;
-            }
-
-            if (revenuePerDay.Length == 0)
-            {// 매출 데이터(요소) 수가 0일 때
                 return -1;
             }
 
@@ -66,7 +57,6 @@
                     dNum += revenuePerDay[i];
                 }
             }
-            
             return dNum;
         }
     }
