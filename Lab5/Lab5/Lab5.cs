bcc620281d8fc4fcb1c92b541576ad454c9a2005
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-02-02 07:50:12 +0900

    no message

--- Lab5/Lab5/Lab5.cs
+++ Lab5/Lab5/Lab5.cs
@@ -5,47 +5,19 @@ namespace Lab5
     public static class Lab5
     {
         public static bool TryFixData(uint[] usersPerDay, double[] revenuePerDay)
-        {//                                  일일 사용자           일일 매출
-            bool bFixed = false;
-            if (usersPerDay.Length != revenuePerDay.Length)
+        {
+            double[] revenuePerDayCopy = new double[revenuePerDay.Length];
+            for (int i = 0; i < revenuePerDay.Length; i++)
             {
-                return false;
+                revenuePerDayCopy[i] = revenuePerDay[i];
             }
 
             for (int i = 0; i < revenuePerDay.Length; i++)
             {
-                if (double.IsNaN(revenuePerDay[i]))
-                {
-                    revenuePerDay[i] = usersPerDay[i];
-                    bFixed = true;
-                }
+                Console.WriteLine(revenuePerDayCopy[i]);
             }
-
-            for (int i = 0; i < revenuePerDay.Length; i++)
-            {
-                if (usersPerDay[i] >= 0 && usersPerDay[i] <= 10)
-                {
-                    revenuePerDay[i] = usersPerDay[i] / 2.0;
-                    revenuePerDay[i] = Math.Round(revenuePerDay[i], 2);
-                }
-                else if (usersPerDay[i] > 10 && usersPerDay[i] <= 100)
-                {
-                    revenuePerDay[i] = 16 * usersPerDay[i] / 5.0 - 27;
-                    revenuePerDay[i] = Math.Round(revenuePerDay[i], 2);
-                }
-                else if (usersPerDay[i] > 100 && usersPerDay[i] <= 1000)
-                {
-                    revenuePerDay[i] = usersPerDay[i] ^ 2 / 4 - 2 * usersPerDay[i] - 2007;
-                    revenuePerDay[i] = Math.Round(revenuePerDay[i], 2);
-                }
-                else if (usersPerDay[i] > 1000)
-                {
-                    revenuePerDay[i] = 245743 + usersPerDay[i] / 4.0;
-                    revenuePerDay[i] = Math.Round(revenuePerDay[i], 2);
-                }
-            }
-
-            return bFixed;
+            
+            return false;
         }
 
 
