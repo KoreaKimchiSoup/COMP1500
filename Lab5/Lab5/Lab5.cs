fd8e41c59e2e9e77e2235b6de0f6998502682a96
DESKTOP-975US36\mh <akehrn13@gamail.com>
2023-02-01 09:08:19 +0900

    no message

--- Lab5/Lab5/Lab5.cs
+++ Lab5/Lab5/Lab5.cs
@@ -4,57 +4,30 @@
     {
         public static bool TryFixData(uint[] usersPerDay, double[] revenuePerDay)
         {//                                  일일 사용자           일일 매출            
-
-            double[] revenuePerDayCopy = new double[revenuePerDay.Length];
-            for (int i = 0; i < revenuePerDayCopy.Length; i++)
-            {
-                revenuePerDayCopy[i] = revenuePerDay[i];
-            }
-
-            for (int i = 0; i < revenuePerDayCopy.Length; i++)
-            {
-                if (revenuePerDay[i] != revenuePerDayCopy[i])
-                {
-                    revenuePerDay[i] = revenuePerDayCopy[i];
-                    return true;
-                }
-            }
             return false;
         }
 
         public static int GetInvalidEntryCount(uint[] usersPerDay, double[] revenuePerDay)
         {
-            if (usersPerDay.Length != revenuePerDay.Length)
-            {
-                return -1;
-            }
-
-            if (usersPerDay.Length == 0 && revenuePerDay.Length == 0)
-            {
-                return -1;
-            }
-
-            int invalidIndex = 0;
-            for (int i = 0; i < usersPerDay.Length; i++)
-            {
-                if (usersPerDay[i] != revenuePerDay[i])
-                {
-                    invalidIndex++;
-                }
-                return invalidIndex;
-            }
-
             return -1;
         }
 
         public static double CalculateTotalRevenue(double[] revenuePerDay, uint start, uint end)
         {
-            if (revenuePerDay.Length == 0)
+            if (revenuePerDay.Length < start)
+            {// 시작, 혹은 끝 index가 매출 데이터 수보다 이상일 때
+                return -1;
+            }
+            else if (revenuePerDay.Length < end)
             {
                 return -1;
             }
-            else if (start < revenuePerDay.Length)
-            {
+            else if (end < start)
+            {// 시작 색인이 끝 색인보다 클 때
+                return -1;
+            }
+            else if (revenuePerDay.Length <= 0)
+            {// 매출 데이터(요소) 수가 0일 때
                 return -1;
             }
             
--- Lab5/Lab5/Program.cs
+++ Lab5/Lab5/Program.cs
@@ -7,7 +7,7 @@ namespace Lab5
     {
         static void Main(string[] args)
         {
-            double[] revenuePerDay = new double[7] { 1.50, 5.00, 184.20, 341.00, 104591.00, 246001.00, 246873.00 };
+            double[] revenuePerDay = new double[-1];
 
             double totalRevenue = Lab5.CalculateTotalRevenue(revenuePerDay, 0, 4); // 105122.7
             Console.WriteLine(totalRevenue);
