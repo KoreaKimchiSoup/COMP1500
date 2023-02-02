b83ed63972f79e42579c365b3af2b6cbcaadb2c7
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-02-03 05:26:52 +0900

    no message

--- Lab5/Lab5/Lab5.cs
+++ Lab5/Lab5/Lab5.cs
@@ -14,28 +14,28 @@ namespace Lab5
                 if (u <= 10)
                 {
                     r = u / 2.0;
-                    Console.WriteLine(r);
                 }
                 else if (u <= 100)
                 {
                     r = ((16 * u) / 5.0) - 27;
-                    Console.WriteLine(r);
                 }
                 else if (u <= 1000)
                 {
                     r = Math.Pow(u, 2) / 4.0 - (2 * u) - 2007;
-                    Console.WriteLine(r);
                 }
                 else if (u > 1000)
                 {
                     r = 245743 + u / 4.0;
-                    Console.WriteLine(r);
                 }
 
                 if (revenuePerDay[i] != r)
                 {
                     bFixed = true;
                     revenuePerDay[i] = r;
+                }
+                else
+                {
+                    bFixed = false;
                 }
 
                 if (revenuePerDay.Length != usersPerDay.Length)
@@ -51,7 +51,7 @@ namespace Lab5
         public static int GetInvalidEntryCount(uint[] usersPerDay, double[] revenuePerDay)
         {
             int invalidEntrys = 0;
-            for (int i = 0; i < usersPerDay.Length; i++)
+            for (int i = 0; i < revenuePerDay.Length; i++)
             {
                 uint u = usersPerDay[i];
                 double r = 0;
@@ -71,7 +71,8 @@ namespace Lab5
                 {
                     r = 245743 + u / 4.0;
                 }
-                else if (revenuePerDay[i] != r)
+
+                if (revenuePerDay[i] != r)
                 {
                     invalidEntrys++;
                 }
