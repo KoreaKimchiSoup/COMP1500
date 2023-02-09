38ce4ed288c963791aa403100e0cbd4fab7aab63
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-02-10 07:52:48 +0900

    no message

--- Lab6/Lab6/Lab6.cs
+++ Lab6/Lab6/Lab6.cs
@@ -9,6 +9,7 @@
 
             int[,] arrayCopy = new int[column, row];
 
+            System.Console.WriteLine("Rotate90Degrees");
             for (int i = 0; i < column; i++)
             {
                 for (int j = 0; j < row; j++)
@@ -22,18 +23,48 @@
 
         public static void TransformArray(int[,] array, EMode eMode)
         {
-            int column = array.GetLength(1);
             int row = array.GetLength(0);
+            int column = array.GetLength(1);
+            int[,] arrayCopy = new int[row, column];
 
-            if (eMode == EMode.HorizontalMirror)
+            if (eMode == EMode.VerticalMirror)
             {
-                int[,] arrayCopy = new int[row, column];
-
+                System.Console.WriteLine("VerticalMirror");
                 for (int i = 0; i < row; i++)
                 {
                     for (int j = 0; j < column; j++)
                     {
-                        arrayCopy[i, j] = array[i, column - 1 - j];
+                        arrayCopy[i, j] = array[row - 1 - i, j];
+                        System.Console.Write("{0, -10}", arrayCopy);
+                    }
+                    System.Console.WriteLine();
+                }
+            }
+
+            if (eMode == EMode.HorizontalMirror)
+            {
+                System.Console.WriteLine("HorizontalMirror");
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < column; j++)
+                    {
+                        //arrayCopy[i, j] = array[column - 1 - j, i];
+                    }
+                }
+            }
+
+            if (eMode == EMode.DiagonalShift)
+            {
+                System.Console.WriteLine("DiagonalShift");
+                for (int i = 0; i < row; i++)
+                {
+                    for (int j = 0; j < column; j++)
+                    {
+                        int rowShift = (i + j + 1) % row;
+                        int columnShift = (j + 1) % column;
+                        //System.Console.Write($"rowShift: {rowShift}       ");
+                        //System.Console.WriteLine($"columnShift: {columnShift}");
+                        arrayCopy[rowShift, columnShift] = array[i, j];
                     }
                 }
             }
