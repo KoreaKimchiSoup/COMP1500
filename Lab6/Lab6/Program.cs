d901b33dadb2b47695e765cac9a7e2f514edd80b
DESKTOP-975US36\mh <akehrn13@gamail.com>
2023-02-11 13:54:50 +0900

    no message

--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -13,10 +13,10 @@ namespace Lab6
                 { 11, 12, 13, 14, 15, 16 },
                 { 21, 22, 23, 24, 25, 26 },
                 { 31, 32, 33, 34, 35, 36 },
-                { 41, 42, 43, 44, 45, 46 }
+                { 41, 42, 43, 44, 45, 44 }
             };
 
-            int[,] rotated = Lab6.Rotate90Degrees(data);
+            //int[,] rotated = Lab6.Rotate90Degrees(data);
         }
     }
 }
