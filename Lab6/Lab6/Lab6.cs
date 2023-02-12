f0060298967cd9c6c781d2a8edd50c65a4a16465
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-02-13 05:17:04 +0900

    no message

--- Lab6/Lab6/Lab6.cs
+++ Lab6/Lab6/Lab6.cs
@@ -4,17 +4,17 @@ namespace Lab6
 {
     public static class Lab6
     {
-        public static int[,] Rotate90Degrees(int[,] data)
+        public static int[,] Rotate90Degrees(int[,] array)
         {
-            int rows = data.GetLength(0);
-            int columns = data.GetLength(1);
+            int rows = array.GetLength(0);
+            int columns = array.GetLength(1);
             int[,] arrayCopy = new int[columns, rows];
 
             for (int i = 0; i < rows; i++)
             {
                 for (int j = 0; j < columns; j++)
                 {
-                    arrayCopy[j, rows - i - 1] = data[i, j];
+                    arrayCopy[j, rows - i - 1] = array[i, j];
                 }
             }
 
@@ -51,7 +51,28 @@ namespace Lab6
 
             else if (mode == EMode.DiagonalShift)
             {
-                
+                for (int i = 0; i < array.GetLength(0) / 2; i++)
+                {
+                    for (int j = 0; j < array.GetLength(1); j++)
+                    {
+                        int temp = array[i, j];
+                        array[i, j] = array[array.GetLength(0) - i - 1, j];
+                        array[array.GetLength(0) - i - 1, j] = temp;
+                    }
+                }
+                /*
+                 {  1,  2,  3,  4,  5,  6 },
+                { 11, 12, 13, 14, 15, 16 },
+                { 21, 22, 23, 24, 25, 26 },
+                { 31, 32, 33, 34, 35, 36 },
+                { 41, 42, 43, 44, 45, 46 }
+
+                46   41   42   43   44   45
+                6     1     2    3     4     5
+                16   11   12   13   14   15
+                26   21   22   23   24   25
+                36   31   32   33   34   35
+                */
             }
         }
     }
--- Lab6/Lab6/Program.cs
+++ Lab6/Lab6/Program.cs
@@ -4,9 +4,17 @@ using System.Diagnostics;
 namespace Lab6
 {
     class Program
+    {
         static void Main(string[] args)
         {
-            int[,] data = getTestArray();
+            int[,] data = new int[5, 6]
+            {
+                {  1,  2,  3,  4,  5,  6 },
+                { 11, 12, 13, 14, 15, 16 },
+                { 21, 22, 23, 24, 25, 26 },
+                { 31, 32, 33, 34, 35, 36 },
+                { 41, 42, 43, 44, 45, 46 }
+            };
 
             int[,] rotated = Lab6.Rotate90Degrees(data);
             printArray(rotated);
