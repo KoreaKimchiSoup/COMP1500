180688641dbc5903d769e4201438441e56c686ce
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-02-28 07:21:28 +0900

    no message

--- Lab7/Lab7/Lab7.cs
+++ Lab7/Lab7/Lab7.cs
@@ -14,9 +14,9 @@
                 System.Console.WriteLine("배열의 길이가 1 이하이면 안됩니다");
                 return false;
             }
-            if (array[0] > array.Length) // 추가
+            if (array[0] >= array.Length) // 추가
             {
-                System.Console.WriteLine("배열의 첫 번째 요소가 배열의 길이보다 크면 안됩니다");
+                System.Console.WriteLine("배열의 첫 번째 요소가 배열의 길이보다 크거나 같으면 안됩니다");
                 return false;
             }
 
@@ -31,17 +31,19 @@
                 System.Console.WriteLine("마지막 요소에 도착하였습니다 클리어!");
                 return true;
             }
+            uint maxJump = array[index];
 
-            uint maxJump = array[index]; // 인덱스 6의 요소인 2 저장
-            if (maxJump > array.Length)
-            {
-                return false;
-            }
             System.Console.WriteLine($"현재 배열의 {index}번 인덱스에 위치해 있으며 요소는 {maxJump} 입니다.");
+            System.Console.WriteLine($"현재 인덱스 {index}에서 {maxJump} 만큼 이동합니다.");
 
             // 배열 범위를 벗어나지 않는 선에서 왼쪽 혹은 오른쪽으로 이동하면서 재귀호출
             for (uint i = 1; i <= maxJump && index + i < array.Length; i++)
             {//                 1 <=        2                     6         + 1 < 10
+                if (index + maxJump > array.Length - 1)
+                {
+                    System.Console.WriteLine("배열의 범위를 벋어났습니다");
+                    return false;
+                }
                 if (PlayGameRecursive(array, index + maxJump))
                 {
                     return true;
