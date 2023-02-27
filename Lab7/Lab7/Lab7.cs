c7cb035a5bef39f3f0716d3e2645cd865d8210c7
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-02-28 05:34:13 +0900

    no message

--- Lab7/Lab7/Lab7.cs
+++ Lab7/Lab7/Lab7.cs
@@ -16,20 +16,20 @@
             return PlayGameRecursive(array, array[0]);
         }
 
-        private static bool PlayGameRecursive(uint[] array, uint currentIndex)
+        public static bool PlayGameRecursive(uint[] array, uint index)
         {
-            if (currentIndex >= array.Length - 1)
+            if (index >= array.Length - 1)
             {
                 // 마지막 요소에 도달한 경우 게임 클리어 가능
                 return true;
             }
 
-            uint maxJump = array[currentIndex]; // 인덱스 6의 요소인 2 저장
+            uint maxJump = array[index]; // 인덱스 6의 요소인 2 저장
 
             // 배열 범위를 벗어나지 않는 선에서 왼쪽 혹은 오른쪽으로 이동하면서 재귀호출
-            for (uint i = 1; i <= maxJump && currentIndex + i < array.Length; i++)
+            for (uint i = 1; i <= maxJump && index + i < array.Length; i++)
             {//                 1 <=        2                     6         + 1 < 10
-                if (PlayGameRecursive(array, currentIndex + i))
+                if (PlayGameRecursive(array, index + i))
                 {
                     return true;
                 }
