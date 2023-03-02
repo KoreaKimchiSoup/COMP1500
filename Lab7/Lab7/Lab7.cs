c46cd87bf988055eeefc3406c24ad3f2ad2a3511
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-03-03 02:09:47 +0900

    no message

--- Lab7/Lab7/Lab7.cs
+++ Lab7/Lab7/Lab7.cs
@@ -2,7 +2,6 @@
 {
     public static class Lab7
     {
-        static uint minusIndex = 0;
         public static bool PlayGame(uint[] array)
         {
             if (array.Length == 0)
@@ -19,28 +18,39 @@
         public static bool CanPlusJump(uint[] array, uint index)
         {
             // 범위를 벗어나는 이동인 경우 false 반환
-            if (index == 0)
+            if (array[array.Length - 1] != 0)
             {
+                System.Console.WriteLine("마지막 색인은 0 이어야만 합니다");
                 return false;
             }
-            if (array[0] < 0)
+
+            if (index == 0)
             {
+                System.Console.WriteLine("배열이 비어있으면 안 됩니다");
+                return false;
+            }
+            if (array[0] <= 0)
+            {
+                System.Console.WriteLine("시작 색인이 0보다 작거나 같을 수 없습니다");
                 return false;
             }
 
             if (index < 0)
             {
+                System.Console.WriteLine("색인이 0보다 작을 수 없습니다");
                 return false;
             }
 
             if (index >= array.Length)
             {
+                System.Console.WriteLine("색인이 배열의 길이보다 크거나 같을 수 없습니다");
                 return false;
             }
 
             // 마지막 요소에 도달한 경우 true 반환
             if (index == array.Length - 1)
             {
+                System.Console.WriteLine("마지막 요소에 도달하였습니다");
                 return true;
             }
 
@@ -50,8 +60,7 @@
 
             if (indexAddArrayIndex > array.Length - 1)
             {
-                minusIndex++;
-                indexAddArrayIndex = array[0] - minusIndex;
+                indexAddArrayIndex = array[0] - 1;
                 //minusIndex 0으로 계속 초기화 되는거 수정 (전역변수로 선언) 
             }
 
@@ -64,9 +73,11 @@
             // 모든 경우에 다음 위치에 도달할 수 없는 경우 false 반환
             return false;
         }
+
+        public static uint MinusIndex(uint minusIndex)
+        {
+            uint returnMinusIndex = 0;
+            return returnMinusIndex++;
+        }
     }
 }
-
-/*
-            
-            */
--- Lab7/Lab7/Program.cs
+++ Lab7/Lab7/Program.cs
@@ -13,43 +13,9 @@ namespace Lab7
 
             Debug.Assert(!bPass);
 
-            array = new uint[1] { 0 };
-            bPass = Lab7.PlayGame(array);
+            array = new uint[10] { 0, 2, 1, 1, 3, 3, 2, 1, 3, 0 };
+            bPass = Lab7.PlayGame(array); // false
             Console.WriteLine(bPass);
-
-            Debug.Assert(!bPass);
-
-            array = new uint[2] { 1, 0 };
-            bPass = Lab7.PlayGame(array);
-            Console.WriteLine(bPass);
-
-            Debug.Assert(bPass);
-
-            array = new uint[4] { 2, 5, 1, 0 };
-            bPass = Lab7.PlayGame(array);
-            Console.WriteLine(bPass);
-
-            Debug.Assert(bPass);
-
-            array = new uint[9] { 4, 3, 4, 4, 1, 5, 7, 1, 0 };
-            bPass = Lab7.PlayGame(array);
-            Console.WriteLine(bPass);
-
-            Debug.Assert(bPass);
-
-            array = new uint[10] { 6, 1, 5, 2, 4, 8, 2, 1, 4, 0 };
-            bPass = Lab7.PlayGame(array);
-            Console.WriteLine(bPass);
-
-            Debug.Assert(!bPass);
-
-            
-            array = new uint[12] { 3, 6, 2, 4, 4, 6, 1, 1, 4, 5, 1, 0 };
-            bPass = Lab7.PlayGame(array);
-            Console.WriteLine(bPass);
-
-            Debug.Assert(bPass);
-            
         }
     }
 }
