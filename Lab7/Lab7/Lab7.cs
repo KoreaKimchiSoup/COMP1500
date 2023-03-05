af7434ac6f39bfdd5f64866a7a369a7dd30be800
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-03-06 00:16:47 +0900

    no message

--- Lab7/Lab7/Lab7.cs
+++ Lab7/Lab7/Lab7.cs
@@ -6,29 +6,72 @@ namespace Lab7
     {
         public static bool PlayGame(uint[] array)
         {
-            if (array == null || array.Length == 0 || array.Length == 1 || array[0] >= array.Length)
+            // 입력받은 배열이 유효한지 검증합니다.
+            if (array.Length < 2)
+            {
+                return false;
+            }
+            if (array[0] >= array.Length)
+            {
+                return false;
+            }
+            if (array[0] == 0)
+            {
+                return false;
+            }
+            if (array[array.Length - 1] != 0)
             {
                 return false;
             }
 
-            return PlayGameHelper(array, (int)array[0], new HashSet<int>());
+            // 재귀함수를 호출하여 게임을 진행합니다.
+            return ArrayJumpRecursive(array, array[0]);
         }
 
-        public static bool PlayGameHelper(uint[] array, int currentIndex, HashSet<int> visitedIndices)
+        public static bool ArrayJumpRecursive(uint[] array, uint index)
         {
-            if (currentIndex < 0 || currentIndex >= array.Length || visitedIndices.Contains(currentIndex))
+            if (index < 0)
             {
                 return false;
             }
 
-            if (array[currentIndex] == 0)
+            // 마지막 위치에 도달한 경우 게임을 클리어하였습니다.
+            if (index == array.Length - 1)
             {
                 return true;
             }
+            // 3, 4, 4, 2, 8, 6, 0
+            index = index + array[index];
 
-            visitedIndices.Add(currentIndex);
-            return PlayGameHelper(array, (int)(currentIndex + array[currentIndex]), visitedIndices)
-                || PlayGameHelper(array, (int)(currentIndex - array[currentIndex]), visitedIndices);
+            if (index > array.Length - 1)
+            {
+                return false;
+            }
+
+            return ArrayJumpRecursive(array, index);
         }
+        /*
+        public static uint LeftJump(uint[] array, uint index)
+        {
+            // { 3, 4, 4, 2, 8, 6, 0 }
+
+            if (index < 0)
+            {
+                return 0;
+            }
+
+            index = index - array[index];
+            uint visitedIndex = 0;
+
+            if (visitedIndex == index)
+            {
+                return 0;
+            }
+
+            visitedIndex = index;
+
+            return index;
+        }
+        */
     }
 }
