69845926feffb5827facb98aa574941a068d9a8d
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-03-06 00:05:04 +0900

    no message

--- Lab7/Lab7/Lab7.cs
+++ Lab7/Lab7/Lab7.cs
@@ -1,104 +1,34 @@
-﻿namespace Lab7
+﻿using System.Collections.Generic;
+
+namespace Lab7
 {
     public static class Lab7
     {
         public static bool PlayGame(uint[] array)
         {
-            // 입력받은 배열이 유효한지 검증합니다.
-            if (array.Length < 2)
-            {
-                return false;
-            }
-            if (array[0] >= array.Length)
-            {
-                return false;
-            }
-            if (array[0] == 0)
-            {
-                return false;
-            }
-            if (array[array.Length - 1] != 0)
+            if (array == null || array.Length == 0 || array.Length == 1 || array[0] >= array.Length)
             {
                 return false;
             }
 
-            // 재귀함수를 호출하여 게임을 진행합니다.
-            return ArrayJumpRecursive(array, array[0]);
+            return PlayGameHelper(array, (int)array[0], new HashSet<int>());
         }
 
-        public static bool ArrayJumpRecursive(uint[] array, uint index)
+        public static bool PlayGameHelper(uint[] array, int currentIndex, HashSet<int> visitedIndices)
         {
-            if (index < 0)
+            if (currentIndex < 0 || currentIndex >= array.Length || visitedIndices.Contains(currentIndex))
             {
                 return false;
             }
 
-            // 마지막 위치에 도달한 경우 게임을 클리어하였습니다.
-            if (index == array.Length - 1)
+            if (array[currentIndex] == 0)
             {
                 return true;
             }
-            // 3, 4, 4, 2, 8, 6, 0
-            index = index + array[index];
 
-            if (index > array.Length - 1)
-            {
-                index = LeftJump(array, array[0]);
-            }
-
-            return ArrayJumpRecursive(array, index);
+            visitedIndices.Add(currentIndex);
+            return PlayGameHelper(array, (int)(currentIndex + array[currentIndex]), visitedIndices)
+                || PlayGameHelper(array, (int)(currentIndex - array[currentIndex]), visitedIndices);
         }
-
-        public static uint LeftJump(uint[] array, uint index)
-        {
-            if (index < 0)
-            {
-                return 0;
-            }
-
-            index = index - array[index];
-            
-            return index;
-        }
-
-        /*
-        public static uint CanMinusJump(uint[] array, uint index)
-        {
-            index
-
-            return index;
-        }
-
-        public static bool CanMinusJump(uint[] array, uint index)
-        {
-            array[0] - array[index - 0]의 값을 리턴함
-            uint returnIndex = array[0] - array[index - 0];
-            if (returnIndex > array.Length - 1)
-            {
-                return false;
-            }
-
-            return true;
-        }
-        */
-
-        /*
-            if (index >= array.Length)
-            {//             3    -    2
-                index = array[0 - array[index - 0]];
-                System.Console.WriteLine(index);
-                if (array[0] - index < 0)
-                {
-                    return false;
-                }
-
-                index = array[0] - index;
-
-                if (index > array.Length - 1)
-                {
-                    return false;
-                }
-            }
-            */
     }
 }
--- Lab7/Lab7/Program.cs
+++ Lab7/Lab7/Program.cs
@@ -9,23 +9,119 @@ namespace Lab7
         {
             uint[] array = new uint[0];
             bool bPass = Lab7.PlayGame(array); // false
-            Console.WriteLine(bPass);
+
+            Debug.Assert(!bPass);
 
             array = new uint[3] { 3, 2, 0 };
             bPass = Lab7.PlayGame(array); // false
-            Console.WriteLine(bPass);
+
+            Debug.Assert(!bPass);
 
             array = new uint[3] { 2, 2, 0 };
             bPass = Lab7.PlayGame(array); // true
-            Console.WriteLine(bPass);
+
+            Debug.Assert(bPass);
 
             array = new uint[3] { 1, 1, 0 };
             bPass = Lab7.PlayGame(array); // true
-            Console.WriteLine(bPass);
 
-            array = new uint[7] { 3, 4, 4, 2, 8, 6, 0 };
+            Debug.Assert(bPass);
+
+            array = new uint[7] { 3, 1, 4, 2, 8, 6, 0 };
             bPass = Lab7.PlayGame(array); // true
-            Console.WriteLine(bPass);
+
+            Debug.Assert(bPass);
+
+            array = new uint[1] { 0 };
+            bPass = Lab7.PlayGame(array); //false
+
+            Debug.Assert(!bPass);
+
+            array = new uint[2] { 1, 0 };
+            bPass = Lab7.PlayGame(array); // true
+
+            Debug.Assert(bPass);
+
+            array = new uint[4] { 2, 5, 1, 0 };
+            bPass = Lab7.PlayGame(array); // true
+
+            Debug.Assert(bPass);
+
+            array = new uint[9] { 4, 3, 4, 4, 1, 5, 7, 1, 0 };
+            bPass = Lab7.PlayGame(array); // true
+
+            Debug.Assert(bPass);
+
+            array = new uint[8] { 2, 2, 2, 2, 1, 2, 2, 0 };
+            bPass = Lab7.PlayGame(array); // true
+
+            Debug.Assert(bPass);
+
+            array = new uint[10] { 6, 1, 5, 2, 4, 8, 2, 1, 4, 0 };
+            bPass = Lab7.PlayGame(array); // false
+
+            Debug.Assert(!bPass);
+
+            array = new uint[12] { 3, 6, 2, 4, 4, 6, 1, 1, 4, 5, 1, 0 };
+            bPass = Lab7.PlayGame(array);  // true
+
+            Debug.Assert(bPass);
+
+            array = new uint[5] { 2, 1, 1, 1, 0 };
+            bPass = Lab7.PlayGame(array);  // true
+
+            Debug.Assert(bPass);
+
+            array = new uint[] { 4, 2, 3, 4, 6, 3, 5, 2, 0 };
+            bPass = Lab7.PlayGame(array);  // false
+
+            Debug.Assert(!bPass);
+
+            array = new uint[] { 5, 4, 1, 2, 1, 2, 0 };
+            bPass = Lab7.PlayGame(array);  // false
+
+            Debug.Assert(!bPass);
+
+            array = new uint[] { 7, 4, 1, 2, 1, 2, 1, 2, 0 };
+            bPass = Lab7.PlayGame(array);  // false
+
+            Debug.Assert(!bPass);
+
+            array = new uint[] { 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0 };
+            bPass = Lab7.PlayGame(array); // true
+
+            Debug.Assert(bPass);
+
+            array = new uint[9] { 4, 2, 3, 4, 6, 3, 5, 2, 0 };
+            bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(!bPass);
+
+            array = new uint[15] { 5, 10, 3, 6, 3, 8, 7, 1, 1, 3, 8, 9, 3, 2, 0 };
+            bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(!bPass);
+
+            array = new uint[40] { 8, 2, 5, 1, 6, 2, 5, 8, 5, 9, 7, 8, 4, 5, 5, 2, 1, 3, 4, 6, 4, 6, 3, 6, 6, 8, 8, 6, 8, 5, 6, 6, 4, 8, 2, 6, 8, 7, 6, 0 };
+            bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(!bPass);
+
+            array = new uint[40] { 6, 2, 1, 4, 1, 9, 7, 2, 3, 7, 1, 9, 5, 1, 6, 8, 6, 8, 1, 5, 4, 2, 7, 9, 6, 3, 9, 1, 6, 1, 1, 2, 7, 5, 3, 9, 5, 4, 7, 0 };
+            bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(bPass);
+
+            array = new uint[20] { 1, 4, 2, 8, 8, 8, 8, 1, 2, 4, 4, 2, 3, 6, 1, 3, 4, 5, 9, 0 };
+            bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(bPass);
+
+            array = new uint[20] { 6, 8, 7, 5, 4, 2, 1, 9, 2, 1, 5, 7, 5, 6, 7, 2, 2, 1, 4, 0 };
+            bPass = Lab7.PlayGame(array);
+
+            Debug.Assert(bPass);
+
 
         }
     }
