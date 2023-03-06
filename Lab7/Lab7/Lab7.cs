f134f1456aa2f84e4e84bd5cf88330479834daf0
DESKTOP-975US36\mh <akehrn13@gamail.com>
2023-03-06 11:08:59 +0900

    no message

--- Lab7/Lab7/Lab7.cs
+++ Lab7/Lab7/Lab7.cs
@@ -9,53 +9,96 @@
             {
                 return false;
             }
-
             if (array[0] >= array.Length)
             {
                 return false;
             }
-
             if (array[0] == 0)
             {
                 return false;
             }
-
             if (array[array.Length - 1] != 0)
             {
                 return false;
             }
 
-            bool[] visitIndex = new bool[array.Length]; 
-
-            return ArrayJumpRecursive(array, array[0], visitIndex);
+            // 재귀함수를 호출하여 게임을 진행합니다.
+            return ArrayJumpRecursive(array, array[0]);
         }
 
-        public static bool ArrayJumpRecursive(uint[] array, uint index, bool[] visitIndex)
+        public static bool ArrayJumpRecursive(uint[] array, uint index)
         {
             if (index < 0)
             {
                 return false;
             }
 
-            if (index >= array.Length)
-            {
-                return false;
-            }
-
-            if (visitIndex[index])
-            {
-                return false;
-            }
-
-            if (array[index] == 0)
+            // 마지막 위치에 도달한 경우 게임을 클리어하였습니다.
+            if (index == array.Length - 1)
             {
                 return true;
             }
+            // 3, 4, 4, 2, 8, 6, 0
+            index = index + array[index];
 
-            visitIndex[index] = true;
+            if (index > array.Length - 1)
+            {
+                index = LeftJump(array, array[0]);
+            }
 
-            return ArrayJumpRecursive(array, (index + array[index]), visitIndex) ||
-                       ArrayJumpRecursive(array, (index - array[index]), visitIndex);
+            return ArrayJumpRecursive(array, index);
         }
+
+        public static uint LeftJump(uint[] array, uint index)
+        {
+            if (index < 0)
+            {
+                return 0;
+            }
+
+            index = index - array[index];
+            
+            return index;
+        }
+
+        /*
+        public static uint CanMinusJump(uint[] array, uint index)
+        {
+            index
+
+            return index;
+        }
+
+        public static bool CanMinusJump(uint[] array, uint index)
+        {
+            array[0] - array[index - 0]의 값을 리턴함
+            uint returnIndex = array[0] - array[index - 0];
+            if (returnIndex > array.Length - 1)
+            {
+                return false;
+            }
+
+            return true;
+        }
+        */
+
+        /*
+            if (index >= array.Length)
+            {//             3    -    2
+                index = array[0 - array[index - 0]];
+                System.Console.WriteLine(index);
+                if (array[0] - index < 0)
+                {
+                    return false;
+                }
+
+                index = array[0] - index;
+
+                if (index > array.Length - 1)
+                {
+                    return false;
+                }
+            }
+            */
     }
 }
--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -7,6 +7,7 @@ namespace Lab8
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            // 123123
         }
     }
 }
