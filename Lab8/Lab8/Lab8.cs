c0e8cfa402e2c8343f6f3cd377716e002ef98eaf
DESKTOP-975US36\mh <akehrn13@gamail.com>
2023-03-06 11:09:38 +0900

    no message

---
+++ Lab8/Lab8/Lab8.cs
@@ -0,0 +1,11 @@
+﻿namespace Lab8
+{
+    public static class Lab8
+    {
+        public static string PrettifyListOrNull(string s)
+        {
+            return null;
+            // 123123
+        }
+    }
+}
