5d33b6fb6bde10ae1615a7f04a57e3ef3f583644
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-03-13 00:40:22 +0900

    no message

--- Lab8/Lab8/Lab8.cs
+++ Lab8/Lab8/Lab8.cs
@@ -13,40 +13,39 @@ namespace Lab8
 
             StringBuilder builder = new StringBuilder(4096);
             string[] tokens1 = s.Split('|');
-            int level1 = 0;
-            int level2 = 0;
-            int level3 = 0;
-            int index = 25;
-            int alphabetIndex = 25;
-            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+            int levelOneIndex = 0;
+            int alphabetIndex = 1;
 
             for (int i = 0; i < tokens1.Length; ++i)
             {
                 string[] tokens2 = tokens1[i].Split('_');
 
-                // 첫 번째 레벨 항목
-                level1++;
-                builder.AppendFormat("{0}) {1}\n", level1, tokens2[0]);
+                levelOneIndex++;
+                builder.AppendFormat("{0}) {1}\n", levelOneIndex, tokens2[0]);
 
                 for (int j = 1; j < tokens2.Length; ++j)
                 {
                     string[] tokens3 = tokens2[j].Split('/');
+                    string alphabet = "";
+                    int temp = alphabetIndex;
 
-                    // 두 번째 레벨 항목
-                    level2++;
-                    builder.AppendFormat("    {0}) {1}\n", alphabet[alphabetIndex % index], tokens3[0]);
+                    while (temp > 0)
+                    {
+                        alphabet = (char)((temp - 1) % 26 + 97) + alphabet; // a ~ z, aa ~ zz ... a.. ~ z..
+                        temp = (temp - 1) / 26;
+                    }
+
+                    builder.AppendFormat("    {0}) {1}\n", alphabet, tokens3[0]);
 
                     for (int k = 1; k < tokens3.Length; ++k)
                     {
-                        // 세 번째 레벨 항목
-                        level3++;
                         builder.AppendFormat("        {0}{1}\n", "- ", tokens3[k]);
                     }
 
                     alphabetIndex++;
                 }
 
-                alphabetIndex = 25;
+                alphabetIndex = 1;
             }
 
             return builder.ToString();
