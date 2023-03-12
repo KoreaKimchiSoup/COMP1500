7b1cd194ed31a90cdd37060fefd0b08a24a7ad7d
DESKTOP-975US36\mh <akehrn13@gamail.com>
2023-03-12 10:53:20 +0900

    no message

--- Lab8/Lab8/Lab8.cs
+++ Lab8/Lab8/Lab8.cs
@@ -1,11 +1,49 @@
-﻿namespace Lab8
+﻿using System.ComponentModel;
+using System.Text;
+
+namespace Lab8
 {
     public static class Lab8
     {
         public static string PrettifyListOrNull(string s)
         {
-            return null;
-            // 123123
+            StringBuilder builder = new StringBuilder(4096);
+            string[] tokens1 = s.Split('|');
+            int level1 = 0;
+            int level2 = 0;
+            int level3 = 0;
+            int index = 25;
+            int alphpbetIndex = 25;
+            char[] alphabet = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+            for (int i = 0; i < tokens1.Length; ++i)
+            {
+                string[] tokens2 = tokens1[i].Split('_');
+
+                // 첫 번째 레벨 항목
+                level1++;
+                builder.AppendFormat("{0}) {1}\n", level1, tokens2[0]);
+
+                for (int j = 1; j < tokens2.Length; ++j)
+                {
+                    string[] tokens3 = tokens2[j].Split('/');
+
+                    // 두 번째 레벨 항목
+                    level2++;
+                    builder.AppendFormat("    {0}) {1}\n", alphabet[alphpbetIndex % index], tokens3[0]);
+
+                    for (int k = 1; k < tokens3.Length; ++k)
+                    {
+                        // 세 번째 레벨 항목
+                        level3++;
+                        builder.AppendFormat("        {0}{1}\n", "- ", tokens3[k]);
+                    }
+
+                    alphpbetIndex++;
+                }
+            }
+
+            return builder.ToString();
         }
     }
 }
--- Lab8/Lab8/Program.cs
+++ Lab8/Lab8/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Text;
+using System.Collections.Generic;
 
 namespace Lab8
 {
@@ -6,8 +8,22 @@ namespace Lab8
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            // 123123
+            string minifiedList = "Week 1_Course Explanation/it's fun/it's awesome_Hello World_Types of Programming Languages|Week 2_Console output_Variables_Primitive Types|Week 3_Casting_Operator_String_Console input";
+
+            string list = Lab8.PrettifyListOrNull(minifiedList);
+            Console.WriteLine(list);
+
+            minifiedList = "Apple///|Orange/asdf";
+            list = Lab8.PrettifyListOrNull(minifiedList);
+            Console.WriteLine(list);
+
+            minifiedList = "Apple|Orange/Blah_mah_nay|";
+            list = Lab8.PrettifyListOrNull(minifiedList);
+            Console.WriteLine(list);
+
+            minifiedList = "Apple|Orange/Blah_mah_nay_34_21_76_23_12_87_21_32_56_88_93_72_58_20_33_42_77_12_07_32_55_61_bah_yay_hey_heh_afd_hgf_rte_fds_ret_uyr|";
+            list = Lab8.PrettifyListOrNull(minifiedList);
+            Console.WriteLine(list);
         }
     }
 }
