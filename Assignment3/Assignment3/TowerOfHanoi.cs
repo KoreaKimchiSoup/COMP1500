40e144c0211c673313cd972aba332a072ad50a09
DESKTOP-975US36\mh <akehrn13@gamail.com>
2023-03-19 11:24:50 +0900

    no message

---
+++ Assignment3/Assignment3/TowerOfHanoi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Assignment3
+{
+    public static class TowerOfHanoi
+    {
+
+        public static int GetNumberOfSteps(int numDiscs)
+        {
+            if (numDiscs < 0)
+            {
+                return -1;
+            }
+
+            return (int)Math.Pow(2, numDiscs) - 1;
+        }
+
+        public static List<List<int>[]> SolveTowerOfHanoi(int numDiscs)
+        {
+            return null;
+        }
+    }
+}
