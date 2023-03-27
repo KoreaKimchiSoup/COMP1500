60ca890c2ce7c9f104809c17a1bd25ccfa7d1845
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-03-28 02:27:49 +0900

    no message

---
+++ Lab11/Lab11/Encoder.cs
@@ -0,0 +1,24 @@
+﻿using System.IO;
+
+namespace Lab11
+{
+    public static class Encoder
+    {
+        public static bool TryEncode(Stream input, Stream output)
+        {
+            if (input == null)
+            {
+                return false;
+            }
+
+
+
+            return true;
+        }
+
+        public static bool TryDecode(Stream input, Stream output)
+        {
+            return false;
+        }
+    }
+}
--- Lab11/Lab11/Program.cs
+++ Lab11/Lab11/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.IO;
 
 namespace Lab11
 {
@@ -6,7 +7,22 @@ namespace Lab11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // data1.txt
+            /*
+            AAAAAAAAFFFFFFDDDDDDDSSSSSSS      ssssss
+            */
+
+            using (Stream read = File.OpenRead("encode1.txt"))
+            using (Stream write = File.OpenWrite("encode1.encoded.bin"))
+            {
+                bool bEncoded = Encoder.TryEncode(read, write); // true
+            }
+
+            // data1.encoded.bin의 이진 데이터
+            /*
+            08 41 06 46 07 44 07 53 06 20 06 73
+            */
         }
     }
 }
+
