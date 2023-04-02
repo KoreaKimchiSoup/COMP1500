b947f5b272f5ac7b5441e0735e91fe5c481a7874
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-04-03 06:07:32 +0900

    no message

---
+++ Lab11/Lab11/Encoder.cs
@@ -0,0 +1,76 @@
+﻿using System.IO;
+
+namespace Lab11
+{
+    public static class Encoder
+    {
+        public static bool TryEncode(Stream input, Stream output)
+        {
+            if (input.Length == 0)
+            {
+                return false;
+            }
+
+            int currentChar = -1;
+            int previousChar = -1;
+            int count = 0;
+
+            while ((currentChar = input.ReadByte()) != -1)
+            {
+                if (currentChar == previousChar)
+                {
+                    count++;
+                    if (count == 255)
+                    {
+                        output.WriteByte((byte)count);
+                        output.WriteByte((byte)previousChar);
+                        count = 1;
+                    }
+                }
+
+                else
+                {
+                    if (previousChar != -1)
+                    {
+                        output.WriteByte((byte)count);
+                        output.WriteByte((byte)previousChar);
+                    }
+
+                    count = 1;
+                }
+
+                previousChar = currentChar;
+            }
+
+            if (count > 0)
+            {
+                output.WriteByte((byte)count);
+                output.WriteByte((byte)previousChar);
+            }
+
+            return true;
+        }
+
+        public static bool TryDecode(Stream input, Stream output)
+        {
+            if (input.Length == 0)
+            {
+                return false;
+            }
+
+            int count;
+            int currentChar;
+
+            while ((count = input.ReadByte()) != -1)
+            {
+                currentChar = input.ReadByte();
+                for (int i = 0; i < count; ++i)
+                {
+                    output.WriteByte((byte)currentChar);
+                }
+            }
+
+            return true;
+        }
+    }
+}
--- Lab11/Lab11/Program.cs
+++ Lab11/Lab11/Program.cs
@@ -1,12 +1,103 @@
-﻿using System;
+﻿using System.Diagnostics;
+using System.IO;
 
 namespace Lab11
 {
     class Program
     {
+        private const string ENCODE_TEST1 = "encode1.txt";
+        private const string ENCODE_TEST2 = "encode2.txt";
+
+        private const string DECODE_TEST1 = "decode1.bin";
+        private const string DECODE_TEST2 = "decode2.bin";
+
+        private const string EMPTY_ENCODE_TEST = "empty_encode.txt";
+        private const string EMPTY_DECODE_TEST = "empty_decode.bin";
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            setup();
+
+            using (Stream read = File.OpenRead(ENCODE_TEST1))
+            using (Stream write = File.OpenWrite($"{ENCODE_TEST1}.output.bin"))
+            {
+                bool bEncoded = Encoder.TryEncode(read, write);
+                // encode1.txt.output.bin과 decode1.bin의 내용이 같아야 함
+                Debug.Assert(bEncoded);
+            }
+
+            using (Stream read = File.OpenRead(DECODE_TEST1))
+            using (Stream write = File.OpenWrite($"{DECODE_TEST1}.output.txt"))
+            {
+                bool bEncoded = Encoder.TryDecode(read, write);
+                // decode1.bin.output.txt과 encode1.txt의 내용이 같아야 함
+                Debug.Assert(bEncoded);
+            }
+
+            using (Stream read = File.OpenRead(ENCODE_TEST2))
+            using (Stream write = File.OpenWrite($"{ENCODE_TEST2}.output.bin"))
+            {
+                bool bEncoded = Encoder.TryEncode(read, write);
+                // encode2.txt.output.bin과 decode2.bin의 내용이 같아야 함
+                Debug.Assert(bEncoded);
+            }
+
+            using (Stream read = File.OpenRead(DECODE_TEST2))
+            using (Stream write = File.OpenWrite($"{DECODE_TEST2}.output.txt"))
+            {
+                bool bEncoded = Encoder.TryDecode(read, write);
+                // decode2.bin.output.txt과 encode2.txt의 내용이 같아야 함
+                Debug.Assert(bEncoded);
+            }
+
+            using (Stream read = File.OpenRead(EMPTY_ENCODE_TEST))
+            using (Stream write = File.OpenWrite($"{EMPTY_ENCODE_TEST}.output.bin"))
+            {
+                bool bEncoded = Encoder.TryEncode(read, write);
+                // empty_encode.txt.output.bin의 내용이 비어 있어야 함
+                Debug.Assert(!bEncoded);
+            }
+
+            using (Stream read = File.OpenRead(EMPTY_DECODE_TEST))
+            using (Stream write = File.OpenWrite($"{EMPTY_DECODE_TEST}.output.txt"))
+            {
+                bool bEncoded = Encoder.TryDecode(read, write);
+                // empty_decode.bin.output.txt의 내용이 비어 있어야 함
+                Debug.Assert(!bEncoded);
+            }
+        }
+
+        private static void setup()
+        {
+            if (File.Exists($"{ENCODE_TEST1}.output.bin"))
+            {
+                File.Delete($"{ENCODE_TEST1}.output.bin");
+            }
+
+            if (File.Exists($"{DECODE_TEST1}.output.txt"))
+            {
+                File.Delete($"{DECODE_TEST1}.output.txt");
+            }
+
+            if (File.Exists($"{ENCODE_TEST2}.output.bin"))
+            {
+                File.Delete($"{ENCODE_TEST2}.output.bin");
+            }
+
+            if (File.Exists($"{DECODE_TEST2}.output.txt"))
+            {
+                File.Delete($"{DECODE_TEST2}.output.txt");
+            }
+
+            if (File.Exists($"{EMPTY_ENCODE_TEST}.output.bin"))
+            {
+                File.Delete($"{EMPTY_ENCODE_TEST}.output.bin");
+            }
+
+            if (File.Exists($"{EMPTY_DECODE_TEST}.output.txt"))
+            {
+                File.Delete($"{EMPTY_DECODE_TEST}.output.txt");
+            }
         }
     }
 }
