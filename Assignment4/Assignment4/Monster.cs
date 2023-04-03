16eb7fe1a7c5de49d62ec5e79ac0b8f9beaabc5b
DESKTOP-BA7TT3V\akehrn13 <akehrn13@gmail.com>
2023-04-03 23:22:42 +0900

    no message

---
+++ Assignment4/Assignment4/Monster.cs
@@ -0,0 +1,21 @@
+﻿namespace Assignment4
+{
+    public class Monster
+    {
+        public string Name { get; private set; }
+        public EElementType EelementType { get; private set; }
+        public int Health { get; private set; }
+        public int AttackStat { get; private set; }
+        public int DefenseStat { get; private set; }
+
+        public Monster(string name, EElementType elementType, int health, int attack, int defense)
+        {
+            Name = name;
+            EelementType = elementType;
+            Health = health;
+            AttackStat = attack;
+            DefenseStat = defense;
+        }
+
+    }
+}
